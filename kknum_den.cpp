c6847666cf53e3c7925fbdcf4d8a7b253800c935
Kavya Kumar <116029008+kavyak1604@users.noreply.github.com>
2023-10-09 21:22:47 +0530

    Add files via upload

---
+++ kknum_den.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+main()
+{ 
+double numerator,denominator;
+cout<<"enter the numerator:";
+cin>>numerator;
+cout<<"enter the denominator:";
+cin>>denominator;
+  try{
+if(denominator==0)
+{ throw("infinite");}
+
+double d=numerator/denominator;
+  cout<<numerator<<"/"<<denominator<<"="<<d<<endl;
+
+}
+
+catch (const char *message)
+{
+	cout<<message;
+}
+}
+/*enter the numerator:6
+enter the denominator:27
+6/27=0.222222
+
