14a8d62480b66d3c5994ed953a3db8d273a5690b
Kavya Kumar <116029008+kavyak1604@users.noreply.github.com>
2023-10-09 21:29:38 +0530

    Add files via upload

---
+++ kktry_throw_catch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	int age;
+	cout<<"enter your age:";
+	cin>>age;
+	try{
+		if(age<18)
+		{
+			try{
+				if(age>=16 &&age<=18)
+				{throw(age);
+				}
+			}
+			catch(int age)
+		{cout<<age<<endl;
+		}
+			throw("you are not authorised to create the account");
+		}
+		cout<<"you are authorised to create the account"<<endl;
+	}
+	catch(const char*msg )
+	{
+		
+		cout<<msg<<endl;
+	}
+}
+/*enter your age:19
+you are authorised to create the account
+/*enter your age:16
+you are not authorised to create the account
+
